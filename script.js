538ef74a01bb5c927a4499cb2dc5914ae1407fb7
Fatih Kincir <kincirfatih@gmail.com>
2022-06-21 13:20:21 +0200

    Quiz constructor created, fetchQuestion() added to prototype, start quiz button created

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function Question(questionText, options, correctAnswer) {
     this.questionText = questionText;
     this.options = options;
     this.correctAnswer = correctAnswer;
-
-    console.log(this)
 }
 
 Question.prototype.checkAnswer = function (answer) {
@@ -16,11 +14,31 @@ let question1 = new Question("which is javascript package management application
 let question2 = new Question("which is .net package management application ? ", { a: "Node.js", b: "Nuget", c: "Npm" }, "b");
 
 let questions = [
-    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
-    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
-    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
-    new Question("which is .net package management application ? ", { a: "Node.js", b: "Nuget", c: "Npm" }, "b")
+    new Question("1-which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("2-which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("3-which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("4-which is .net package management application ? ", { a: "Node.js", b: "Nuget", c: "Npm" }, "b")
 
 ];
+function Quiz(questions) {
+    this.questions = questions;
+    this.questionIndex = 0;
+}
 
-console.log(question1.checkAnswer("c"))
+Quiz.prototype.fetchQuestion = function () {
+    return this.questions[this.questionIndex]
+}
+
+const quiz = new Quiz(questions);
+
+console.log(quiz.fetchQuestion());
+
+document.querySelector(".btn-start").addEventListener("click", function () {
+    if (quiz.questions.length != quiz.questionIndex) {
+
+        console.log(quiz.fetchQuestion())
+        quiz.questionIndex += 1;
+    } else {
+        console.log("there is no more question")
+    }
+})
