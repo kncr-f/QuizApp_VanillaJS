8d54518d614ff19392b253e7cd1cf4ebb8f26edb
Fatih Kincir <kincirfatih@gmail.com>
2022-06-21 12:49:51 +0200

    Question Constructor created

---
+++ script.js
@@ -0,0 +1,25 @@
+//OOP: Object Oriented Programming
+
+function Question(questionText, options, correctAnswer) {
+    this.questionText = questionText;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+    this.checkAnswer = function (answer) {
+        return answer === this.correctAnswer
+    }
+
+
+}
+
+let question1 = new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c");
+let question2 = new Question("which is .net package management application ? ", { a: "Node.js", b: "Nuget", c: "Npm" }, "b");
+
+let questions = [
+    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("which is javascript package management application ? ", { a: "Node.js", b: "Typscript", c: "Npm" }, "c"),
+    new Question("which is .net package management application ? ", { a: "Node.js", b: "Nuget", c: "Npm" }, "b")
+
+];
+
+console.log(question1.checkAnswer("c"))
